88173daea87a8f864981e3b6c49e5d7cd4673694
NottPeak <105991975+NottPeak@users.noreply.github.com>
2024-05-04 21:13:04 +0000

    .

--- Ultraviolet-Static/public/nowgg.js
+++ Ultraviolet-Static/public/nowgg.js
@@ -1,5 +1,5 @@
 function nowgg() {
     let nowggIFrame = document.querySelector(".active > iframe");
     nowggIFrame.onload = clickIt;
-    function clickIt() {nowggIFrame.contentDocument.getElementById("ng-getting-ready-btn").click()}
+    function clickIt() {nowggIFrame.contentDocument.getElementById("ng-getting-ready-btn").click();setTimeout(alert("You may need to scroll down"),1500)}
 }
